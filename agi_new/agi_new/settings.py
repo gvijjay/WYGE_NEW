bf881cf0bd770e36124f9136ddef906270b033f2
gvijjay <gunti.vijay@gmail.com>
2025-03-14 18:10:27 +0530

    Update settings.py

--- agi_new/agi_new/settings.py
+++ agi_new/agi_new/settings.py
@@ -50,7 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # CORS middleware
+    'corsheaders.middleware.CorsMiddleware',  # Add this line
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'agi_new.urls'
